829bf34685c998d26d1596fb3af39c796a2d69f3
TarunChandra <tarunmanepally4@gmail.com>
2024-07-03 01:47:50 +0530

    Created hello world with React and parcel bundle only

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const h1 = document.createElement("h1")
-h1.innerHTML="Hello world"
-const root = document.getElementById("root")
-root.appendChild(h1)
+import React from "react"
+import ReactDOM from "react-dom/client"
+const child1 = React.createElement("h1",{id:"child1"},"Child1")
+const child2 = React.createElement("h1",{id:"child2"},"Child2")
+const parent = React.createElement("div",{id:"parent"},[child1,child2])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
