b9377f6ee52f332e74568f71febaf81a4df6b600
TarunChandra <tarunmanepally4@gmail.com>
2024-07-03 20:36:39 +0530

    Added components

---
+++ Components/BodyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BodyComponent = () => {
+  return (
+    <div>
+      <h1>Body1</h1>
+    </div>
+  );
+}
+
+export default BodyComponent;
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
 
-// const h1 = React.createElement("h1",{id:"h1"},"Hello Tarun Welcome")
-
-const h1 = <h1 id="h1">Hello tarun from jsx</h1> // This is jsx syntax to write code in html like syntsx
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(h1)
+const App=()=>{
+    return (
+        <div className="app">
+            <Header/>
+            {/* <Body/>
+            {/* <Footer/> */}
+        </div>
+    )
+}
+export default App
